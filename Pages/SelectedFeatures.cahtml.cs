79fb15a3544b4b4c7f7e8a19eb7cf9b582524451
Alexey Drozdovsky <AlexeyD@neogames.com>
2023-04-05 22:00:42 +0300

    added selected features page

---
+++ Pages/SelectedFeatures.cahtml.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Enteties;
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
+using TFSViewer.BusinessLogic;
+
+namespace TFSViewer.Pages;
+
+public class SelectedFeatures : PageModel
+{
+
+    public void OnGet()
+    {
+        
+    }
+
+}
+
